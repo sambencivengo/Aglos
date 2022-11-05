56615ee2a8dbcac8d7f837b5281505f2383406e7
Sam Bencivengo <sambencivengo@gmail.com>
2022-11-05 17:48:48 -0600

    map for maxChar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,27 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+	const map = new Map();
+	let max = 0;
+	let maxChar;
+
+	str.split('').forEach((char) => {
+		const element = map.get(char);
+		if (!element) {
+			map.set(char, 1);
+		} else {
+			map.set(char, element + 1);
+		}
+	});
+
+	for (const el of map) {
+		if (el[1] > max) {
+			max = el[1];
+			maxChar = el[0];
+		}
+	}
+	return maxChar;
+}
 
 module.exports = maxChar;
