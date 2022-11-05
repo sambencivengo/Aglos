bb68b2e965f7e5ccb75dd4d9af5816ba1d6e000e
Sam Bencivengo <sambencivengo@gmail.com>
2022-11-05 13:16:14 -0600

    palindrome every

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,34 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+// function palindrome(str) {
+// 	// LONG AN SLOW
+// 	return str.split('').reverse().join('') === str;
+// }
+
+// function palindrome(str) {
+// 	let start = 0;
+// 	let end = str.length - 1;
+
+// 	while (start < end) {
+// 		if (str[start] === str[end]) {
+// 			start++;
+// 			end--;
+// 		} else {
+// 			return false;
+// 		}
+// 	}
+// 	return true;
+// }
+
+// a, b, c, b, a
+// 0, 1, 2, 3, 4
+
+//  a(0)         a(4 - 0)
+//  b(1)         b(4 - 1)
+//  c(2)         c(4 - 2)
+function palindrome(str) {
+	return str.split('').every((char, i) => char === str[str.length - 1 - i]);
+}
 
 module.exports = palindrome;
