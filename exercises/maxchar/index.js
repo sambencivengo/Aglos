e8123c41bb7e8ed61d375d91818e1b04a93cbb33
Sam Bencivengo <sambencivengo@gmail.com>
2022-11-05 17:53:22 -0600

    alternate solution... but it's the same with a slightly different syntax for incrementing a key in a map

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,23 +5,46 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
+// function maxChar(str) {
+// 	const map = new Map();
+// 	let max = 0;
+// 	let maxChar;
+
+// 	str.split('').forEach((char) => {
+// 		const element = map.get(char);
+// 		if (!element) {
+// 			map.set(char, 1);
+// 		} else {
+// 			map.set(char, element + 1);
+// 		}
+// 	});
+
+// 	for (const el of map) {
+// 		if (el[1] > max) {
+// 			max = el[1];
+// 			maxChar = el[0];
+// 		}
+// 	}
+// 	return maxChar;
+// }
+
+// ALTERNATE SYNTAX FOR VALUE
 function maxChar(str) {
 	const map = new Map();
 	let max = 0;
 	let maxChar;
 
 	str.split('').forEach((char) => {
-		const element = map.get(char);
-		if (!element) {
-			map.set(char, 1);
+		if (!map.get(char)) {
+			map.set(char, { value: 1 });
 		} else {
-			map.set(char, element + 1);
+			map.get(char).value++;
 		}
 	});
 
 	for (const el of map) {
-		if (el[1] > max) {
-			max = el[1];
+		if (el[1].value > max) {
+			max = el[1].value;
 			maxChar = el[0];
 		}
 	}
