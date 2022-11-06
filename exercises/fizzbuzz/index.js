2ef995167a0a501d7fa2f2751a9ae003b0dd5134
Sam Bencivengo <sambencivengo@gmail.com>
2022-11-06 10:24:57 -0700

    simple fb

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -12,6 +12,35 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
+// function fizzBuzz(n) {
+// 	let num = 1;
+// 	while (num <= n) {
+// 		if (num % 15 === 0) {
+// 			console.log('fizzbuzz');
+// 		} else if (num % 5 === 0) {
+// 			console.log('buzz');
+// 		} else if (num % 3 === 0) {
+// 			console.log('fizz');
+// 		} else {
+// 			console.log(num);
+// 		}
+// 		num++;
+// 	}
+// }
+function fizzBuzz(n) {
+	let num = 1;
+	while (num <= n) {
+		if (num % 15 === 0) {
+			console.log('fizzbuzz');
+		} else if (num % 5 === 0) {
+			console.log('buzz');
+		} else if (num % 3 === 0) {
+			console.log('fizz');
+		} else {
+			console.log(num);
+		}
+		num++;
+	}
+}
 
 module.exports = fizzBuzz;
