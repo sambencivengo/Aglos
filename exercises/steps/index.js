29600e1cef0161d1d53423b91b6e335c28c604a9
Sam Bencivengo <sambencivengo@gmail.com>
2022-11-06 19:40:32 -0700

    steps

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,29 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps(n) {
+	for (let row = 0; row < n; row++) {
+		let stair = '';
+		// each loop is 1 row
+		// inner loop will print each character for all columns before moving to next row
+
+		for (let col = 0; col < n; col++) {
+			// loops n times for each time the outer loop runs once
+			// will move along the columns until we hit the length of n, then exits into outer loop for next row
+			// FIRST LOOP
+			// col and row are both zero
+			// col is less than or equal to row so we add a step
+
+			//
+			if (col <= row) {
+				stair += '#';
+			} else {
+				stair += ' ';
+			}
+		}
+		console.log({ stair });
+		console.log(stair);
+	}
+}
 
 module.exports = steps;
